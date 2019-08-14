c4d3ed8129ebf9916dde87729970337f6ab8dbdb
fandonglai <704072802@qq.com>
2019-08-14 08:54:37 +0800

    fix-on

--- day-2/src/directives/clickOutside.js
+++ day-2/src/directives/clickOutside.js
@@ -1,18 +1,28 @@
 
+// const listener = function (e) {
+//   return (el, bindings) => {
+//     if (e.target === el || el.contains(e.target)) {
+//       return;
+//     }
+//     bindings.value(); // close事件
+//   };
+// };
 
 export default { // 指令是一个方法  指令有自己的生命周期
   // bind update
+
   clickOutside: {
     inserted(el, bindings) { // el真实的dom元素
-      document.addEventListener('click', (e) => {
+      el.listener = function listener(e) {
         if (e.target === el || el.contains(e.target)) {
           return;
         }
         bindings.value(); // close事件
-      });
+      };
+      document.addEventListener('click', el.listener);
     },
-    // unbind() { // 在指令卸载的时候 执行此操作
-    //   //   document.removeEventListener('click', listener);
-    // },
+    unbind(el) {
+      document.removeEventListener('click', el.listener);
+    },
   },
 };
